553a7a5ebb26f077cfea4140744df68d7db194ab
Duy Pham <duypham@Duys-MacBook-Pro.local>
2020-09-23 19:04:18 +0700

    Creating the Services Component

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/pages/Footer/Footer";
 import Home from "./components/pages/HomePage/Home";
+import Services from "./components/pages/Services/Services";
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
             <Navbar />
             <Switch>
                 <Route path="/" exact component={Home} />
+                <Route path="/services" component={Services} />
             </Switch>
             <Footer />
         </Router>
