1d460ce3448a2f837729294aa9b3fd57c26853cc
Duy Pham <duypham@Duys-MacBook-Pro.local>
2020-09-23 18:20:19 +0700

    Adding the Footer Component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/pages/Footer/Footer";
 import Home from "./components/pages/HomePage/Home";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
             <Switch>
                 <Route path="/" exact component={Home} />
             </Switch>
+            <Footer />
         </Router>
     );
 }
--- src/components/pages/HomePage/Data.js
+++ src/components/pages/HomePage/Data.js
@@ -41,9 +41,9 @@ export const homeObjThree = {
 };
 
 export const homeObjFour = {
-    lightBg: false,
-    lightText: true,
-    lightTextDesc: true,
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
     topLine: "DATA ANALYTICS",
     headline: "Every transaction is stored on our secure cloud database",
     description:
