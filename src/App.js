f5e9caf413951967d8876767f64899afd81c4c5f
Duy Pham <duypham@Duys-MacBook-Pro.local>
2020-09-23 18:09:37 +0700

    Creating the Homepage

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "./App.css";
 
 import Navbar from "./components/Navbar";
+import Home from "./components/pages/HomePage/Home";
 
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path="/" />
-      </Switch>
-    </Router>
-  );
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route path="/" exact component={Home} />
+            </Switch>
+        </Router>
+    );
 }
 
 export default App;
